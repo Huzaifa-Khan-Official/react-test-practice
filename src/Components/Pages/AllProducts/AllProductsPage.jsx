d01d3fc75d9e1cef4f216b80950d39339a1f9354
Huzaifa Khan <m.huzaifakhan2004@gmail.com>
2024-04-26 00:45:27 +0500

    real time data listner done

--- src/Components/Pages/AllProducts/AllProductsPage.jsx
+++ src/Components/Pages/AllProducts/AllProductsPage.jsx
@@ -1,13 +1,35 @@
-import React, { useEffect } from 'react'
+// import React, { useEffect, useState } from 'react'
+// import { getAllProducts } from '../../../Services/products.service'
+
+// export default function AllProductsPage() {
+//     const [products, setProducts] = useState(null);
+
+//     useEffect(() => {
+//         (async () => {
+//             const productsResponse = await getAllProducts();
+//             console.log("productsResponse ==>", productsResponse);
+//         })()
+//     }, [])
+//     return (
+//         <div>All Products Page</div>
+//     )
+// }
+
+import React, { useEffect, useState } from 'react'
 import { getAllProducts } from '../../../Services/products.service'
 
 export default function AllProductsPage() {
+    const [products, setProducts] = useState(null);
+
     useEffect(() => {
-        (async () => {
-            const productsResponse = await getAllProducts();
-            console.log("productsResponse ==>", productsResponse);
-        })()
-    }, [])
+        const unsubscribe = getAllProducts((productsList) => {
+            console.log("productsList ==>", productsList);
+            setProducts(productsList);
+        });
+
+        return () => unsubscribe();
+    }, []);
+
     return (
         <div>All Products Page</div>
     )
--- src/Services/products.service.js
+++ src/Services/products.service.js
@@ -1,12 +1,19 @@
-import { collection, query, where, onSnapshot, getDoc, getDocs } from "firebase/firestore";
+import { collection, onSnapshot } from "firebase/firestore";
 import { db } from "../Configurations/FirbaseConfiguration/firebase.config";
 import { productEntity } from "../lib/productEntities";
 
-export const getAllProducts = async () => {
-    const q = collection(db, productEntity);
+export const getAllProducts = (callback) => {
+    try {
+        const q = collection(db, productEntity);
 
-    const querySnapshot = await getDocs(q);
-    querySnapshot.forEach((doc) => {
-        console.log("doc ==>", doc.data());
-    });
+        return onSnapshot(q, (snapshot) => {
+            const productsList = snapshot.docs.map((doc) => {
+                return doc.data();
+            });
+            callback(productsList);
+        });
+
+    } catch (error) {
+        console.error("Error fetching products:", error);
+    }
 }
